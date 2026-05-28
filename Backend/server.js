49151d74142309d91c863fadd475c2e3ee2b581a
Tye1 <dophantruongtien@gmail.com>
2026-05-28 19:57:39 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://www.dophantruongtien.io.vn"
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/fido", fidoRoutes);
